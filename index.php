82a7f844f8e3d25d14558bee5c706d1491723a38
Cristopher Rodriguez <59444688+CristopherMRodriguez@users.noreply.github.com>
2020-03-05 00:09:04 -0500

    initial loadup

---
+++ index.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="style.css">
+    <!-- Bootstrap CSS -->
+    <link 
+        rel="stylesheet" 
+        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" 
+        integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" 
+        crossorigin="anonymous"
+    >
+    <link rel="icon" href="gnr-art/slash.jpg" sizes="32x32">
+    </head>
+
+    <body class="bg-dark">
+    
+        <div class="banner-div text-center">        
+            <img src="gnr-art/108774.jpg" alt="Responsive image">
+        </div>
+
+        <div class="container text-muted">
+
+            <!-- cards -->
+            <div class="row mt-4">
+                <div class="col-md-6 col-lg-4">
+                    <div class="card">
+                        <img class="card-img-top img-fluid" src="gnr-art/axl.jpg">
+                        <div class="card-block">
+                            <h3 class="card-title">Axl Rose</h3>
+                            <p class="card-text">Love him or hate him, Axl is an iconic singer for the band. Known for his shrill voice and controversial antics, Axl may be the perfect frontman for GnR.
+                                
+
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-4">
+                    <div class="card">
+                        <img class="card-img-top img-fluid" src="gnr-art/slash.jpg">
+                        <div class="card-block">
+                            <h3 class="card-title">Slash</h3>
+                            <p class="card-text">The perfect right hand man. His signature look and sound completed the band and he is recognized as one of the greatest guitarists of all time.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-4">
+                    <div class="card">
+                        <img class="card-img-top img-fluid" src="gnr-art/izzy.jpg">
+                        <div class="card-block">
+                            <h3 class="card-title">Izzy Stradlin</h3>
+                            <p class="card-text">The other guitarist for GnR, Izzy contributed to the thrashing rhythms that made the band famous.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-4">
+                    <div class="card">
+                        <img class="card-img-top img-fluid" src="gnr-art/adler.jpg">
+                        <div class="card-block">
+                            <h3 class="card-title">Steven Adler</h3>
+                            <p class="card-text">A rock band is only as good as their drummer.  After Steven left the band, GnR lamented the band was never the same.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-4">
+                    <div class="card">
+                        <img class="card-img-top img-fluid" src="gnr-art/duff.jpg">
+                        <div class="card-block">
+                            <h3 class="card-title">Duff McKagan</h3>
+                            <p class="card-text">Driving the groove of the band as bassist, Duff also was featured as backup vocalist.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- navs -->
+        <div class="meet-band">    
+            <h2 class="display-4 text-white text-center py-5 my-4">Meet the Band</h2>
+            <nav class="nav justify-content-center pb-5">
+                <a class="nav-link" href="#axl">Axl</a>
+                <a class="nav-link" href="#slash">Slash</a>
+                <a class="nav-link" href="#steven">Steven Adler</a>
+                <a class="nav-link" href="#izzy">Izzy Stradlin</a>
+                <a class="nav-link" href="#duff">Duff McKagan</a>
+            </nav>
+
+            <iframe 
+                class="axl-vid" 
+                id="axl"
+                width="560" 
+                height="315" 
+                src="https://www.youtube.com/embed/Gwd0O9Ca6ts" 
+                frameborder="0" 
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+                allowfullscreen>
+            </iframe>
+
+            <iframe 
+                class="slash-vid"
+                id="slash"
+                width="560" 
+                height="315" 
+                src="https://www.youtube.com/embed/cCIULTlTpII" 
+                frameborder="0" 
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+                allowfullscreen>
+            </iframe>
+
+            <iframe 
+                class="steven-vid"
+                id="steven"
+                width="560" 
+                height="315" 
+                src="https://www.youtube.com/embed/fidY49ad4QY" 
+                frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+                allowfullscreen>
+            </iframe>
+
+            <iframe 
+                class="izzy-vid"
+                id="izzy"
+                width="560" 
+                height="315" 
+                src="https://www.youtube.com/embed/lFRJZU9Of9s?start=40" 
+                frameborder="0" 
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+                allowfullscreen>
+            </iframe>
+
+            <iframe 
+                class="duff-vid"
+                id="duff"
+                width="560" 
+                height="315" src="https://www.youtube.com/embed/gj4ingFOZgM" 
+                frameborder="0" 
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+                allowfullscreen>
+            </iframe>
+
+        </div>
+
+        <div>
+            <img class="img-end" src="gnr-art/41335.jpg"
+        </div>
+
+   
+    </body>
+        
+</html>
